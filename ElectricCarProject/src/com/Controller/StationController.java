5ab0449cfd512299d97e6bd92a2622d0dd9bc77f
kosta <kosta@kosta-HP>
2017-09-26 15:19:47 +0900

    aa

--- ElectricCarProject/src/com/Controller/StationController.java
+++ ElectricCarProject/src/com/Controller/StationController.java
@@ -10,21 +10,11 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class StationController
  */
 public class StationController extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public StationController() {
-        super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	
 	}
 
 }
